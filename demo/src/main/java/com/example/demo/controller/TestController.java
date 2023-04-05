5ca9f1f0363bca066a0b8419f7bef33a3e739a12
hkkim85 <hkkim85@icloud.com>
2023-04-06 02:57:51 +0900

    frontend 배포

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,18 +1,13 @@
 package com.example.demo.controller;
 
-import org.slf4j.Logger;
-import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins = "http://59.5.105.145:8081")
-//@Slf4j
-@RestController
+@Controller
 public class TestController {
 	
-	@GetMapping("/")
-	public String index() {
-		return "스프링 부트&뷰 연동 테스트!!";
+	@GetMapping("/home")
+	public String homepage() {
+		return "home";
 	}
 }
